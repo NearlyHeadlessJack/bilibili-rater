c3959c3689b7d6c928879978db1cca4d5b60ff26
NearlyHeadlessJack <wang@rjack.cn>
2026-01-21 23:16:13 +0800

    add: cinemagoer

---
+++ bilibili_rater/_internal/imdb/cinemagoer_imdb.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
+# author： NearlyHeadlessJack
+# email: wang@rjack.cn
+# datetime： 2026/1/21 22:41 
+# ide： PyCharm
+# file: cinemagoer_imdb.py
+from imdb import Cinemagoer
+
